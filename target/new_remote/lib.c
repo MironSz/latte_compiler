fd8d55fb2e0e5ff5bbd60ef6810742cdd1b9921f
mironszewczyk <m.szewczyk@vooom.pl>
2021-01-16 15:07:38 +0100

    Fixed reading int

--- target/new_remote/lib.c
+++ target/new_remote/lib.c
@@ -4,22 +4,26 @@
 
 #define LL long long
 
+
 extern void c_exit(LL a){
     exit((int) a);
 }
 extern  void c_print_int(LL a){
-    printf("%lld\n" , a);
+    printf("%d\n" , (int)a);
 }
 
 
 extern  LL c_read_int() {
     LL a;
-    scanf("%lld" , &a);
+    scanf("%lld\n" , &a);
     return a;
 }
 
 extern  void c_print_str(void * s){
-    printf("%s" , (char *) (s+sizeof(LL )));
+    LL length = * (LL *) (s);
+    ((char *)(s+sizeof(LL)))[*((LL *) s)]='\n';
+
+    printf("%.*s\n" , *((LL *) s), (char *) (s+sizeof(LL )));
 }
 
 extern  void * c_read_str(){
@@ -34,10 +38,12 @@ extern  void * c_read_str(){
     memcpy(result_ptr+sizeof(LL ), buffor_ptr, length+1);
 
     free(buffor_ptr);
+    printf("Read: ");
+    c_print_str(result_ptr);
     return result_ptr;
 }
 
-extern  void *  c_add_two_strings(void * s2, void * s1){
+extern  void *  c_add_two_strings(void * s1, void * s2){
     LL length1  = *( LL *) s1;
     LL length2  = *( LL *) s2;
     LL result_length = length1+length2;
